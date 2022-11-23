2c315a2266e3ea4d8ac7d99436c935982dd52321
diegogalanti <dggalanti@hotmail.com>
2022-11-23 18:56:38 +0100

    Included extension function to convert domain model into DB entity and included a timestamp property

--- app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingList.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingList.kt
@@ -1,9 +1,14 @@
 package com.gallardo.shoppinglist.domain.model
 
+import com.gallardo.shoppinglist.data.database.model.ShoppingListEntity
+
 data class ShoppingList(
     val name: String,
     val description: String?,
     val color: Int,
     val type: String,
+    val timestamp: Long,
     val id: Int
 )
+
+fun ShoppingList.asDatabaseEntity() = ShoppingListEntity(name, description, color, type, timestamp, id)
