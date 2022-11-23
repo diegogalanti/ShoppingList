44ad35dece9badde8dc9b1e8e0a23f72708f459d
diegogalanti <dggalanti@hotmail.com>
2022-11-23 11:48:40 +0100

    Got the database name from a BuildConfig file

--- app/src/main/java/com/gallardo/shoppinglist/data/database/ShoppingListDatabase.kt
+++ app/src/main/java/com/gallardo/shoppinglist/data/database/ShoppingListDatabase.kt
@@ -1,19 +1,22 @@
 package com.gallardo.shoppinglist.data.database
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.gallardo.shoppinglist.BuildConfig
 import com.gallardo.shoppinglist.data.database.dao.ShoppingListDao
+import com.gallardo.shoppinglist.data.database.dao.ShoppingListItemDao
 import com.gallardo.shoppinglist.data.database.model.ShoppingListEntity
 import com.gallardo.shoppinglist.data.database.model.ShoppingListItemEntity
 
 @Database(entities = [ShoppingListEntity::class, ShoppingListItemEntity::class], version = 1)
-abstract class ShoppingListDatabase {
+abstract class ShoppingListDatabase: RoomDatabase() {
 
     abstract val shoppingListDao : ShoppingListDao
 
-    abstract val shoppingListItemDao : ShoppingListDao
+    abstract val shoppingListItemDao : ShoppingListItemDao
 
     companion object {
-        const val DATABASE_NAME = "shopping_list_db"
+        const val DATABASE_NAME = BuildConfig.DATABASE_NAME
     }
 
 }
