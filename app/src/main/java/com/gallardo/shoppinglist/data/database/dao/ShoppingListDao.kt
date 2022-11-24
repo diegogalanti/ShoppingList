12e20e511792b4d13af336619073361ce1648f8b
diegogalanti <dggalanti@hotmail.com>
2022-11-24 15:36:22 +0100

    Change from Insert to Upsert

--- app/src/main/java/com/gallardo/shoppinglist/data/database/dao/ShoppingListDao.kt
+++ app/src/main/java/com/gallardo/shoppinglist/data/database/dao/ShoppingListDao.kt
@@ -1,10 +1,6 @@
 package com.gallardo.shoppinglist.data.database.dao
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.gallardo.shoppinglist.data.database.model.ShoppingListEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -16,8 +12,8 @@ interface ShoppingListDao {
     @Query("SELECT * FROM ShoppingListEntity WHERE id = :id")
     suspend fun getShoppingList(id: Int): ShoppingListEntity
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertShoppingList(list: ShoppingListEntity)
+    @Upsert
+    suspend fun upsertShoppingList(list: ShoppingListEntity)
 
     @Delete
     suspend fun deleteShoppingList(list: ShoppingListEntity)
