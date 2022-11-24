989031307bfcff5c07e12cd94e2c6c14558d61a3
diegogalanti <dggalanti@hotmail.com>
2022-11-24 15:37:30 +0100

    Change quantity to Float and id to nullable Int

--- app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItem.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItem.kt
@@ -4,11 +4,11 @@ import com.gallardo.shoppinglist.data.database.model.ShoppingListItemEntity
 
 data class ShoppingListItem(
     val description: String,
-    val quantity: Int?,
+    val quantity: Float?,
     val unit: String?,
     val done: Boolean,
     val listId: Int,
-    val id: Int
+    val id: Int?
 )
 
 fun ShoppingListItem.asDatabaseEntity() = ShoppingListItemEntity(description, quantity, unit, done, listId, id)
