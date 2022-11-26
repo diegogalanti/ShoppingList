1f68130b2db2f156dd719f6a68ebcafed25d8a1b
diegogalanti <dggalanti@hotmail.com>
2022-11-26 23:23:59 +0100

    Material theme files, adjustments on the Theme to adjust status and navigation bar colors

--- app/src/main/java/com/gallardo/shoppinglist/presentation/theme/Type.kt
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/theme/Type.kt
@@ -2,19 +2,59 @@ package com.gallardo.shoppinglist.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.gallardo.shoppinglist.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+    ),
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.Default
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default
     )
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
