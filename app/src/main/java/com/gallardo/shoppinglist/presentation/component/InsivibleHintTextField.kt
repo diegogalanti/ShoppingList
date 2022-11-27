4050454617b51d79bc94dc87d0d2e796cd24f686
diegogalanti <dggalanti@hotmail.com>
2022-11-27 23:49:07 +0100

    BasicTextField Implementation with a hint when empty

---
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/component/InsivibleHintTextField.kt
@@ -0,0 +1,37 @@
+package com.gallardo.shoppinglist.presentation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun TransparentHintTextField(
+    text: String,
+    hint: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+    textStyle: TextStyle = TextStyle(),
+    singleLine: Boolean = false,
+) {
+    Box(
+        modifier = modifier
+    ) {
+        BasicTextField(
+            value = text,
+            onValueChange = onValueChange,
+            singleLine = singleLine,
+            textStyle = textStyle,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        if(text.isBlank()) {
+            Text(text = hint, style = textStyle, color = Color.Gray)
+        }
+    }
+}
