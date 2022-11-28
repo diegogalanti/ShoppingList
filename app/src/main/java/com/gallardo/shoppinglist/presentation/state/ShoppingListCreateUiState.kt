17b23f4c2080e369a2d0e29d0a975d970bbdd424
diegogalanti <dggalanti@hotmail.com>
2022-11-28 19:33:48 +0100

    Color and type start as a random value, list saved replaced by shouldClose which can be used both when the list was saved or not.

--- app/src/main/java/com/gallardo/shoppinglist/presentation/state/ShoppingListCreateUiState.kt
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/state/ShoppingListCreateUiState.kt
@@ -2,13 +2,15 @@ package com.gallardo.shoppinglist.presentation.state
 
 import com.gallardo.shoppinglist.domain.model.ShoppingListItemSimple
 import com.gallardo.shoppinglist.domain.model.UserMessage
+import com.gallardo.shoppinglist.presentation.component.PaperSheetColor
+import com.gallardo.shoppinglist.presentation.component.PaperSheetStyle
 
 data class ShoppingListCreateUiState(
     val name: String = "",
     val description: String? = "",
-    val color: Int = 1,
-    val type: Int? = 1,
+    val color: Int = PaperSheetColor.randomColor().ordinal,
+    val type: Int? = PaperSheetStyle.randomStyle().ordinal,
     val itemList: List<ShoppingListItemSimple> = listOf(ShoppingListItemSimple()),
     val userMessages: List<UserMessage> = emptyList(),
-    val listSaved: Boolean = false
+    val shouldClose: Boolean = false
 )
