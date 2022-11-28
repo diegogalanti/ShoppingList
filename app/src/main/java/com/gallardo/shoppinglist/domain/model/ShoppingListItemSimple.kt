3ddc476bbec9a484ab2eb0e6e6396cdcc9c53d97
diegogalanti <dggalanti@hotmail.com>
2022-11-28 16:41:43 +0100

    Removed item unit property as it was too much for the screen, changed quantity to string to allow more flexibility.

--- app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItemSimple.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItemSimple.kt
@@ -3,9 +3,8 @@ package com.gallardo.shoppinglist.domain.model
 
 data class ShoppingListItemSimple(
     val description: String = "",
-    val quantity: Float? = null,
-    val unit: String? = null
+    val quantity: String? = "1"
 )
 
-fun ShoppingListItemSimple.asShoppingList(listID: Int) = ShoppingListItem(description, quantity, unit, false, listID, null)
+fun ShoppingListItemSimple.asShoppingList(listID: Int) = ShoppingListItem(description, quantity, false, listID, null)
 
