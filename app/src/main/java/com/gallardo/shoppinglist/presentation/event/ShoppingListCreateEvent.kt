20cc9cde935bbb4a7e3d32b428c5de26ffb3fbe8
diegogalanti <dggalanti@hotmail.com>
2022-11-28 15:18:57 +0100

    Removed item Unit event, it was too much for the screen, changed quantity to string to allow more flexibility and included an event to the close event.

--- app/src/main/java/com/gallardo/shoppinglist/presentation/event/ShoppingListCreateEvent.kt
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/event/ShoppingListCreateEvent.kt
@@ -6,7 +6,7 @@ sealed interface ShoppingListCreateEvent {
     data class DescriptionChangeEvent(val description: String): ShoppingListCreateEvent
     data class TypeChangeEvent(val type: Int): ShoppingListCreateEvent
     object ListSaveEvent: ShoppingListCreateEvent
+    object ListCloseEvent: ShoppingListCreateEvent
     data class ItemDescriptionChangeEvent(val index: Int, val description: String): ShoppingListCreateEvent
-    data class ItemUnitChangeEvent(val index: Int, val unit: String): ShoppingListCreateEvent
-    data class ItemQuantityChangeEvent(val index: Int, val quantity: Float): ShoppingListCreateEvent
+    data class ItemQuantityChangeEvent(val index: Int, val quantity: String): ShoppingListCreateEvent
 }
