3d443aeadd293cbcb69096e0909e0cb14aca3615
diegogalanti <dggalanti@hotmail.com>
2022-11-28 16:58:36 +0100

    Component that represents a line in a shopping list

---
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/component/ShoppingListPaperSheetLine.kt
@@ -0,0 +1,44 @@
+package com.gallardo.shoppinglist.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShoppingListPaperSheetLine(
+    description: String,
+    quantity: String,
+    onDescriptionChange: (String) -> Unit,
+    onQuantityChange: (String) -> Unit
+) {
+    Row(horizontalArrangement = Arrangement.End) {
+        Text(" • ", color = Color(0xFF373acb))
+        BasicTextField(
+            modifier = Modifier.weight(1f),
+            value = description,
+            onValueChange = onDescriptionChange,
+            textStyle = MaterialTheme.typography.bodyLarge.copy(
+                color = Color(
+                    0xFF373acb
+                )
+            ),
+        )
+        BasicTextField(
+            value = quantity,
+            onValueChange = onQuantityChange,
+            textStyle = MaterialTheme.typography.bodyLarge.copy(
+                color = Color(
+                    0xFF373acb
+                ), textAlign = TextAlign.Center
+            ), modifier = Modifier.width(70.dp)
+        )
+    }
+}
