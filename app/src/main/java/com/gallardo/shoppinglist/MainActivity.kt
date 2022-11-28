08f9cce43baec6cb3f4efcc58513750518f41630
diegogalanti <dggalanti@hotmail.com>
2022-11-28 14:54:36 +0100

    Hilt annotation and Navigation components on the main Activity

--- app/src/main/java/com/gallardo/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/gallardo/shoppinglist/MainActivity.kt
@@ -3,41 +3,18 @@ package com.gallardo.shoppinglist
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.gallardo.shoppinglist.presentation.navigation.ShoppingListNav
 import com.gallardo.shoppinglist.presentation.theme.ShoppingListTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ShoppingListNav()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ShoppingListTheme {
-        Greeting("Android")
     }
 }
