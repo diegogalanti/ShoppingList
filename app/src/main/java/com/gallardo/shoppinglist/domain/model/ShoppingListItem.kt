5bc0ed946941c50be0c7ee51ec24691d74b505df
diegogalanti <dggalanti@hotmail.com>
2022-11-28 16:39:46 +0100

    Removed item Unit event, it was too much for the screen, changed quantity to string to allow more flexibility.

--- app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItem.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItem.kt
@@ -4,11 +4,10 @@ import com.gallardo.shoppinglist.data.database.model.ShoppingListItemEntity
 
 data class ShoppingListItem(
     val description: String,
-    val quantity: Float?,
-    val unit: String?,
+    val quantity: String?,
     val done: Boolean,
     val listId: Int,
     val id: Int?
 )
 
-fun ShoppingListItem.asDatabaseEntity() = ShoppingListItemEntity(description, quantity, unit, done, listId, id)
+fun ShoppingListItem.asDatabaseEntity() = ShoppingListItemEntity(description, quantity, done, listId, id)
