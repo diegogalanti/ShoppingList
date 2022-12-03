eab3382277df99a86c34a661bf496979e2eb5090
diegogalanti <dggalanti@hotmail.com>
2022-12-03 18:17:46 +0100

    Spacing scheme and extension parameter to link spacing data like it was native Material Design data.

---
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/theme/Spacing.kt
@@ -0,0 +1,24 @@
+package com.gallardo.shoppinglist.presentation.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class SpacingScheme(
+    val standard: Dp = 0.dp,
+    val extraSmall: Dp = 4.dp,
+    val small: Dp = 8.dp,
+    val medium: Dp = 16.dp,
+    val large: Dp = 32.dp,
+    val extraLarge: Dp = 64.dp
+)
+
+val LocalSpacingScheme = compositionLocalOf { SpacingScheme() }
+
+val MaterialTheme.spacingScheme: SpacingScheme
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalSpacingScheme.current
