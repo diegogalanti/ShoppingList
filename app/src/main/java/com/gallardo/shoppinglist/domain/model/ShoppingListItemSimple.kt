3bcc3d22740de1f30807388d3359fcae03597d82
diegogalanti <dggalanti@hotmail.com>
2022-12-03 18:19:47 +0100

    Removed initial values and left this to the ViewModel/Use cases to implement.

--- app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItemSimple.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/model/ShoppingListItemSimple.kt
@@ -2,8 +2,8 @@ package com.gallardo.shoppinglist.domain.model
 
 
 data class ShoppingListItemSimple(
-    val description: String = "",
-    val quantity: String? = "1"
+    val description: String,
+    val quantity: String?
 )
 
 fun ShoppingListItemSimple.asShoppingList(listID: Int) = ShoppingListItem(description, quantity, false, listID, null)
