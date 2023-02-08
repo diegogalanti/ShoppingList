ba60d01f57bd708eb267499f12084fb92ea00067
Diego Galanti Gallardo <dggalanti@hotmail.com>
2023-02-08 12:12:29 +0100

    feature: SwipeToDismiss implementation for Shopping list screen. Had to change items() extension function so each item is identified by id and not by position, so the dismissState don't consider the next item on the list to be dismissed after the item removal from the list.

--- app/src/main/java/com/gallardo/shoppinglist/presentation/screen/ShoppingListScreen.kt
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/screen/ShoppingListScreen.kt
@@ -1,5 +1,8 @@
 package com.gallardo.shoppinglist.presentation.screen
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -7,14 +10,24 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.gallardo.shoppinglist.presentation.component.PaperSheetColor
+import com.airbnb.lottie.compose.*
+import com.gallardo.shoppinglist.R
+import com.gallardo.shoppinglist.domain.model.ShoppingList
 import com.gallardo.shoppinglist.presentation.component.PaperSheetStyle
+import com.gallardo.shoppinglist.presentation.component.PaperSheetTexture
+import com.gallardo.shoppinglist.presentation.component.PenColor
 import com.gallardo.shoppinglist.presentation.component.ShoppingListPaperSheetSummary
+import com.gallardo.shoppinglist.presentation.event.ShoppingListEvent
 import com.gallardo.shoppinglist.presentation.state.ShoppingListUiState
+import com.gallardo.shoppinglist.presentation.theme.dark_red
+import com.gallardo.shoppinglist.presentation.theme.light_red
 import com.gallardo.shoppinglist.presentation.view_model.ShoppingListViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -51,18 +64,65 @@ fun ShoppingListScreen(modifier: Modifier = Modifier, onFabClick: () -> Unit) {
                             .padding(paddingValue)
                             .padding(16.dp)
                     ) {
-                        items(items = state.shoppingLists) { currentList ->
-                            ShoppingListPaperSheetSummary(
-                                titleValue = currentList.name,
-                                descriptionValue = currentList.description?:"",
-                                paperColor = PaperSheetColor.values()[currentList.color],
-                                paperStyle = PaperSheetStyle.values()[currentList.type]
-                            )
+                        items(
+                            items = state.shoppingLists,
+                            { listItem: ShoppingList -> listItem.id!! }) { currentList ->
+                            var dismissState = rememberDismissState(positionalThreshold = {
+                                density * 70
+                            })
+                            if (dismissState.isDismissed(DismissDirection.StartToEnd)) {
+                                viewModel.onEvent(ShoppingListEvent.DeleteListEvent(currentList))
+                            }
+                            SwipeToDismiss(
+                                state = dismissState,
+                                directions = setOf(DismissDirection.StartToEnd),
+                                background = {
+                                    val color by animateColorAsState(
+                                        targetValue = when (dismissState.targetValue) {
+                                            DismissValue.Default -> MaterialTheme.colorScheme.background
+                                            else -> if (isSystemInDarkTheme()) dark_red else light_red
+                                        }
+                                    )
+                                    val composition by rememberLottieComposition(
+                                        if (isSystemInDarkTheme()) LottieCompositionSpec.RawRes(
+                                            R.raw.ic_trash_dk
+                                        ) else LottieCompositionSpec.RawRes(R.raw.ic_trash)
+                                    )
+                                    val progress by animateLottieCompositionAsState(
+                                        composition,
+                                        isPlaying = when (dismissState.targetValue) {
+                                            DismissValue.Default -> false
+                                            else -> true
+                                        },
+                                        cancellationBehavior = LottieCancellationBehavior.OnIterationFinish
+                                    )
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxSize()
+                                            .background(color)
+                                            .padding(start = 10.dp),
+                                        verticalAlignment = Alignment.CenterVertically,
+                                        horizontalArrangement = Arrangement.Start
+                                    ) {
+                                        LottieAnimation(
+                                            composition = composition,
+                                            progress = { progress },
+                                            modifier = modifier.height(35.dp)
+                                        )
+                                    }
+                                }, dismissContent = {
+                                    ShoppingListPaperSheetSummary(
+                                        titleValue = currentList.name,
+                                        descriptionValue = currentList.description ?: "",
+                                        paperTexture = PaperSheetTexture.values()[currentList.paperTexture],
+                                        paperStyle = PaperSheetStyle.values()[currentList.type],
+                                        penColor = PenColor.values()[currentList.penColor],
+                                    )
+                                })
                             Spacer(modifier = Modifier.height(8.dp))
                         }
                     }
             }
         }
     }
-
 }
