3112ce9cfad1e8977f4ef1cca68eb3fa40b28a38
Diego Galanti Gallardo <dggalanti@hotmail.com>
2023-02-10 12:17:07 +0100

    refactor: Adjusted typography with suggested sized from matherial 3 design.

--- app/src/main/java/com/gallardo/shoppinglist/presentation/theme/Type.kt
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/theme/Type.kt
@@ -4,71 +4,84 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 import com.gallardo.shoppinglist.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 64.sp,
+        fontSize = 57.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 52.sp,
+        fontSize = 45.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 44.sp,
+        fontSize = 36.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 40.sp,
+        fontSize = 32.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 36.sp,
+        fontSize = 28.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 32.sp,
+        fontSize = 24.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 28.sp,
+        fontSize = 22.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 24.sp,
+        fontSize = 16.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 20.sp,
+        fontSize = 14.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 24.sp,
+        fontSize = 16.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 20.sp,
+        fontSize = 14.sp
     ),
     bodySmall = TextStyle(
-        fontFamily = FontFamily(Font(R.font.crafty_girls_regular))
+        fontFamily = FontFamily(Font(R.font.crafty_girls_regular)),
+        lineHeight = 16.sp,
+        fontSize = 12.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = FontFamily.Default
+        fontFamily = FontFamily.Default,
+        lineHeight = 20.sp,
+        fontSize = 14.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = FontFamily.Default
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default
-    )
-
-
-    /* Other default text styles to override
-    titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        lineHeight = 16.sp,
+        fontSize = 11.sp
     )
-    */
 )
