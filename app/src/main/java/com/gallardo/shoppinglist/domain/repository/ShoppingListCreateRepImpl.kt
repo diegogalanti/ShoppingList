f93a601f7f4bc3e9b211255e5d40d4225749b8f2
Diego Galanti Gallardo <dggalanti@hotmail.com>
2023-02-10 12:18:25 +0100

    refactor: better separation of concerns for the itemDao and listDao

--- app/src/main/java/com/gallardo/shoppinglist/domain/repository/ShoppingListCreateRepImpl.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/repository/ShoppingListCreateRepImpl.kt
@@ -1,20 +1,21 @@
 package com.gallardo.shoppinglist.domain.repository
 
+import com.gallardo.shoppinglist.data.database.dao.ShoppingListDao
 import com.gallardo.shoppinglist.data.database.dao.ShoppingListItemDao
 import com.gallardo.shoppinglist.domain.model.ShoppingList
 import com.gallardo.shoppinglist.domain.model.ShoppingListItem
 import com.gallardo.shoppinglist.domain.model.asDatabaseEntity
 
-class ShoppingListCreateRepImpl(private val dao: ShoppingListItemDao): ShoppingListCreateRep {
+class ShoppingListCreateRepImpl(private val listDao: ShoppingListDao, private val itemDao: ShoppingListItemDao): ShoppingListCreateRep {
     override suspend fun createShoppingList(list: ShoppingList) : Long {
-        return dao.upsertShoppingList(list.asDatabaseEntity())
+        return listDao.upsertShoppingList(list.asDatabaseEntity())
     }
 
     override suspend fun upsertShoppingListItem(items: List<ShoppingListItem>) {
-        dao.upsertShoppingListItem(items.map { it.asDatabaseEntity() })
+        itemDao.upsertShoppingListItems(items.map { it.asDatabaseEntity() })
     }
 
     override suspend fun getShoppingListID(rowID: Long): Int {
-        return dao.getShoppingListID(rowID)
+        return listDao.getShoppingListID(rowID)
     }
 }
--- app/src/main/java/com/gallardo/shoppinglist/domain/repository/ShoppingListRepImpl.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/repository/ShoppingListRepImpl.kt
@@ -1,30 +1,36 @@
 package com.gallardo.shoppinglist.domain.repository
 
 import com.gallardo.shoppinglist.data.database.dao.ShoppingListDao
+import com.gallardo.shoppinglist.data.database.dao.ShoppingListItemDao
 import com.gallardo.shoppinglist.data.database.model.asDomainModel
 import com.gallardo.shoppinglist.domain.model.ShoppingList
+import com.gallardo.shoppinglist.domain.model.ShoppingListItem
 import com.gallardo.shoppinglist.domain.model.asDatabaseEntity
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
-class ShoppingListRepImpl(private val dao : ShoppingListDao) : ShoppingListRep {
+class ShoppingListRepImpl(private val listDao : ShoppingListDao, private val itemDao : ShoppingListItemDao) : ShoppingListRep {
     override fun getShoppingLists(): Flow<List<ShoppingList>> {
-        return dao.getShoppingLists().map {
+        return listDao.getShoppingLists().map {
             it.map { currentList ->
                 currentList.asDomainModel()
             }
         }
     }
 
-    override suspend fun getShoppingList(id: Int): ShoppingList {
-        return dao.getShoppingList(id).asDomainModel()
+    override suspend fun getShoppingListItems(listId: Int): List<ShoppingListItem> {
+        return itemDao.getShoppingListItems(listId).map { it.asDomainModel() }
     }
 
     override suspend fun insertShoppingList(list: ShoppingList) {
-        dao.upsertShoppingList(list.asDatabaseEntity())
+        listDao.upsertShoppingList(list.asDatabaseEntity())
+    }
+
+    override suspend fun insertShoppingListItems(items: List<ShoppingListItem>) {
+        itemDao.upsertShoppingListItems(items.map { it.asDatabaseEntity() })
     }
 
     override suspend fun deleteShoppingList(list: ShoppingList) {
-        dao.deleteShoppingList(list.asDatabaseEntity())
+        listDao.deleteShoppingList(list.asDatabaseEntity())
     }
 }
