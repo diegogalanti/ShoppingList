e8e0e1049624b679bc9292fb2af30151c03d1ace
Diego Galanti Gallardo <dggalanti@hotmail.com>
2023-02-10 12:19:37 +0100

    bugfix: Fixed a bug where recovering a list from deletion would not recover its items.

--- app/src/main/java/com/gallardo/shoppinglist/domain/repository/ShoppingListRep.kt
+++ app/src/main/java/com/gallardo/shoppinglist/domain/repository/ShoppingListRep.kt
@@ -1,14 +1,17 @@
 package com.gallardo.shoppinglist.domain.repository
 
 import com.gallardo.shoppinglist.domain.model.ShoppingList
+import com.gallardo.shoppinglist.domain.model.ShoppingListItem
 import kotlinx.coroutines.flow.Flow
 
 interface ShoppingListRep {
     fun getShoppingLists() : Flow<List<ShoppingList>>
 
-    suspend fun getShoppingList(id: Int): ShoppingList
+    suspend fun getShoppingListItems(listId: Int) : List<ShoppingListItem>
 
     suspend fun insertShoppingList(list: ShoppingList)
+
+    suspend fun insertShoppingListItems(items: List<ShoppingListItem>)
 
     suspend fun deleteShoppingList(list: ShoppingList)
 }
--- app/src/main/java/com/gallardo/shoppinglist/presentation/view_model/ShoppingListViewModel.kt
+++ app/src/main/java/com/gallardo/shoppinglist/presentation/view_model/ShoppingListViewModel.kt
@@ -3,39 +3,47 @@ package com.gallardo.shoppinglist.presentation.view_model
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gallardo.shoppinglist.domain.model.ShoppingList
+import com.gallardo.shoppinglist.domain.model.ShoppingListItem
 import com.gallardo.shoppinglist.domain.repository.ShoppingListRep
 import com.gallardo.shoppinglist.presentation.event.ShoppingListEvent
 import com.gallardo.shoppinglist.presentation.state.ShoppingListUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ShoppingListViewModel @Inject constructor( private val repository: ShoppingListRep) : ViewModel() {
+class ShoppingListViewModel @Inject constructor(private val repository: ShoppingListRep) :
+    ViewModel() {
 
-    val uiState : StateFlow<ShoppingListUiState> = repository.getShoppingLists().map {
-        ShoppingListUiState.Success(it)
+    val uiState: StateFlow<ShoppingListUiState> = repository.getShoppingLists().map { list ->
+        ShoppingListUiState.Success(list.sortedByDescending { item ->
+            item.timestamp
+        })
     }.stateIn(
         scope = viewModelScope,
         initialValue = ShoppingListUiState.Loading,
         started = SharingStarted.WhileSubscribed()
     )
-
     private var deletedList: ShoppingList? = null
-
+    private var deletedItems: List<ShoppingListItem>? = null
     fun onEvent(event: ShoppingListEvent) {
         when (event) {
             is ShoppingListEvent.DeleteListEvent -> {
-                viewModelScope.launch {
-                    repository.deleteShoppingList(event.list)
+                viewModelScope.launch(Dispatchers.IO) {
+                    deletedItems = event.list.id?.let { repository.getShoppingListItems(it) }
                     deletedList = event.list
+                    repository.deleteShoppingList(event.list)
                 }
             }
             is ShoppingListEvent.UndoDeleteListEvent -> {
-                deletedList?.let {
-                    viewModelScope.launch {
+                viewModelScope.launch(Dispatchers.IO) {
+                    deletedList?.let {
                         repository.insertShoppingList(it)
+                    }
+                    deletedItems?.let {
+                        repository.insertShoppingListItems(it)
                     }
                 }
             }
